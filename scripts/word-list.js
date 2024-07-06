22bbb56a0b7b68efe137f7413c25e685f7661f9c
alifseptiandii <164166617+septiandyy@users.noreply.github.com>
2024-07-06 16:00:33 +0700

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,262 @@
+const wordList = [
+    {
+        word: "seeker",
+        hint: "What position does Harry play on his Quidditch team?"
+    },
+    {
+        word: "dumbledore",
+        hint: "Who is the headmaster of Hogwarts for most of the series?"
+    },
+    {
+        word: "gryffindor",
+        hint: "What house at Hogwarts does Harry belong to?"
+    },
+    {
+        word: "hedwig",
+        hint: "What is the name of Harry's owl?"
+    },
+    {
+        word: "voldemort",
+        hint: "What is the name of the dark wizard Harry battles?"
+    },
+    {
+        word: "scabbers",
+        hint: "What is the name of Ron Weasley's pet rat?"
+    },
+    {
+        word: "expelliarmus",
+        hint: "What spell is used to disarm an opponent?"
+    },
+    {
+        word: "time-turner",
+        hint: "What magical device does Hermione use to attend multiple classes at once?"
+    },
+    {
+        word: "sirius",
+        hint: "Who is Harry's godfather?"
+    },
+    {
+        word: "dementor",
+        hint: "What are the creatures that guard Azkaban prison?"
+    },
+    {
+        word: "felix",
+        hint: "What potion grants good luck?"
+    },
+    {
+        word: "whomping",
+        hint: "What is the name of the tree that attacks anyone who comes near it?"
+    },
+    {
+        word: "dursley",
+        hint: "What is the surname of the family Harry lives with?"
+    },
+    {
+        word: "nagini",
+        hint: "What is the name of Voldemort's snake?"
+    },
+    {
+        word: "burrow",
+        hint: "What is the name of the Weasley's house?"
+    },
+    {
+        word: "snape",
+        hint: "Who is the Half-Blood Prince?."
+    },
+    {
+        word: "trelawney",
+        hint: "Who teaches Divination?"
+    },
+    {
+        word: "lily",
+        hint: "What is Harry's mother's name?"
+    },
+    {
+        word: "binns",
+        hint: "Who is the author of the 'History of Magic'?"
+    },
+    {
+        word: "griphook",
+        hint: "What is the name of the goblin who helps Harry?"
+    },
+    {
+        word: "snape",
+        hint: "Who is the Defense Against the Dark Arts teacher in the sixth book?"
+    },
+    {
+        word: "erised",
+        hint: "What is the name of the mirror that shows desires?"
+    },
+    {
+        word: "filch",
+        hint: "Who is the caretaker of Hogwarts?"
+    },
+    {
+        word: "azkaban",
+        hint: "What is the name of the wizard prison?"
+    },
+    {
+        word: "cho",
+        hint: "Who is Harry's first kiss?"
+    },
+    {
+        word: "patronus",
+        hint: "What is the name of the charm that repels Dementors?"
+    },
+    {
+        word: "ballet",
+        hint: "A classical dance form characterized by precise and graceful movements."
+    },
+    {
+        word: "olivianders",
+        hint: "What is the name of the shop where Harry buys his wand?"
+    },
+    {
+        word: "ministry",
+        hint: "What is the magical government in Britain?"
+    },
+    {
+        word: "myrtle",
+        hint: "What is the name of the ghost in the girls' bathroom?"
+    },
+    {
+        word: "scamander",
+        hint: "Who is the author of 'Magical Beasts'?"
+    },
+    {
+        word: "lucius",
+        hint: "What is the name of Draco Malfoy's father?"
+    },
+    {
+        word: "mcgonagall",
+        hint: "Who teaches Transfiguration?"
+    },
+    {
+        word: "arthur",
+        hint: "What is the name of Ron's father?"
+    },
+    {
+        word: "hippogriff",
+        hint: "What creature is Buckbeak?"
+    },
+    {
+        word: "bellatrix",
+        hint: "What is the name of Voldemort's loyal follower?"
+    },
+    {
+        word: "otter",
+        hint: "What is Hermione's patronus?"
+    },
+    {
+        word: "ginny",
+        hint: "Who did Harry marry?"
+    },
+    {
+        word: "baron",
+        hint: "Who is the ghost of Slytherin House?"
+    },
+    {
+        word: "pensieve",
+        hint: "What magical object is used to store memories?"
+    },
+    {
+        word: "sprout",
+        hint: "Who is the head of Hufflepuff House?"
+    },
+    {
+        word: "fawkes",
+        hint: "What is the name of Dumbledore's phoenix?"
+    },
+    {
+        word: "flitwick",
+        hint: "Who is the head of Ravenclaw House?"
+    },
+    {
+        word: "accio",
+        hint: "What is the name of the charm to summon objects?"
+    },
+    {
+        word: "fatlady",
+        hint: "What creature guards the entrance to the Gryffindor common room?"
+    },
+    {
+        word: "dark",
+        hint: "What is the name of the Death Eater's mark?"
+    },
+    {
+        word: "sprout",
+        hint: "Who is the herbology professor?"
+    },
+    {
+        word: "stag",
+        hint: "What is Harry's patronus?"
+    },
+    {
+        word: "kreacher",
+        hint: "What is the name of the house elf who serves the Black family?"
+    },
+    {
+        word: "lovegood",
+        hint: "Who is the editor of the Quibbler?"
+    },
+    {
+        word: "alohomora",
+        hint: "What spell is used to unlock doors?"
+    },
+    {
+        word: "lumos",
+        hint: "What is the name of the charm that creates light?"
+    },
+    {
+        word: "Wood",
+        hint: "Who is the Gryffindor Quidditch captain in Harry's first year?"
+    },
+    {
+        word: "levitate",
+        hint: "What does the spell 'Wingardium Leviosa' do?"
+    },
+    {
+        word: "quirrell",
+        hint: "Who is the first Defense Against the Dark Arts teacher?"
+    },
+    {
+        word: "riddikulus",
+        hint: "What is the name of the creature that Boggarts transform into?"
+    },
+    {
+        word: "gringotts",
+        hint: "What is the name of the wizard bank?"
+    },
+    {
+        word: "lavender",
+        hint: "Who does Ron date in the sixth book?"
+    },
+    {
+        word: "scrimgeour",
+        hint: "Who is the head of the Auror Office?"
+    },
+    {
+        word: "grimmauld",
+        hint: "What is the name of the house where Sirius Black grew up?"
+    },
+    {
+        word: "thestral",
+        hint: "What magical creature can only be seen by those who have witnessed death?"
+    },
+    {
+        word: "ariana",
+        hint: "What is the full name of Dumbledore's younger sister?"
+    },
+    {
+        word: "felix",
+        hint: "What is the name of the potion that grants the drinker temporary luck?"
+    },
+    {
+        word: "",
+        hint: "What is the name of the spell used to erase memories?"
+    },
+    {
+        word: "salazar",
+        hint: "What is the full name of the founder of Slytherin House?"
+    },
+];
